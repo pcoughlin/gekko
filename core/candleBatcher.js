46031af004d42726131a6e06a60d79b9852242d9
Rahul Patil <rahulp884@gmail.com>
2020-05-09 15:07:17 +0530

    Update candle batcher
    - Update to use es6 format

--- core/candleBatcher.js
+++ core/candleBatcher.js
@@ -12,82 +12,184 @@
 var _ = require('lodash');
 var util = require(__dirname + '/util');
 
-var CandleBatcher = function(candleSize) {
-  if(!_.isNumber(candleSize))
-    throw new Error('candleSize is not a number');
+// var CandleBatcher (candleSize) {
+//   if (!_.isNumber(candleSize))
+//     throw new Error('candleSize is not a number');
 
-  this.candleSize = candleSize;
-  this.smallCandles = [];
-  this.calculatedCandles = [];
+//   this.candleSize = candleSize;
+//   this.smallCandles = [];
+//   this.calculatedCandles = [];
 
-  _.bindAll(this);
-}
+//   _.bindAll(this);
+// }
 
-util.makeEventEmitter(CandleBatcher);
+// util.makeEventEmitter(CandleBatcher);
 
-CandleBatcher.prototype.write = function(candles) {
-  if(!_.isArray(candles)) {
-    throw new Error('candles is not an array');
+// check () {
+//   if (_.size(this.smallCandles) % this.candleSize !== 0)
+//     return;
+
+//   this.emitted++;
+//   this.calculatedCandles.push(this.calculate());
+//   this.smallCandles = [];
+// }
+
+
+// write (candles) {
+//   if (!_.isArray(candles)) {
+//     throw new Error('candles is not an array');
+//   }
+
+//   this.emitted = 0;
+
+//   _.each(candles, function (candle) {
+//     if (this.smallCandles) {
+//       this.smallCandles.push(candle);
+//     } else {
+//       this.smallCandles = [candle];
+//     }
+//     this.check();
+//   }, this);
+
+//   return this.emitted;
+// }
+
+
+// flush () {
+//   _.each(
+//     this.calculatedCandles,
+//     candle => this.emit('candle', candle)
+//   );
+
+//   this.calculatedCandles = [];
+// }
+
+// calculate () {
+//   // remove the id property of the small candle
+//   var { id, ...first } = this.smallCandles.shift();
+
+//   first.vwp = first.vwp * first.volume;
+
+//   var candle = _.reduce(
+//     this.smallCandles,
+//     function (candle, m) {
+//       candle.high = _.max([candle.high, m.high]);
+//       candle.low = _.min([candle.low, m.low]);
+//       candle.close = m.close;
+//       candle.volume += m.volume;
+//       candle.vwp += m.vwp * m.volume;
+//       candle.trades += m.trades;
+//       return candle;
+//     },
+//     first
+//   );
+
+//   if (candle.volume)
+//     // we have added up all prices (relative to volume)
+//     // now divide by volume to get the Volume Weighted Price
+//     candle.vwp /= candle.volume;
+//   else
+//     // empty candle
+//     candle.vwp = candle.open;
+
+//   candle.start = first.start;
+//   return candle;
+// }
+
+class CandleBatcher {
+
+  // candleSize = 0;
+  // smallCandles = [];
+  // calculatedCandles = [];
+
+  constructor(candleSizeInput) {
+    console.log("*************************************", candleSizeInput);
+    
+    if (!_.isNumber(candleSizeInput))
+      throw new Error('candleSize is not a number');
+
+    this.candleSize = candleSizeInput;
+    this.smallCandles = [];
+    this.calculatedCandles = [];
+    _.bindAll(this);
+    util.makeEventEmitter(CandleBatcher);
   }
 
-  this.emitted = 0;
 
-  _.each(candles, function(candle) {
-    this.smallCandles.push(candle);
-    this.check();
-  }, this);
+  check() {
+    if (_.size(this.smallCandles) % this.candleSize !== 0)
+      return;
 
-  return this.emitted;
-}
+    this.emitted++;
+    this.calculatedCandles.push(this.calculate());
+    this.smallCandles = [];
+  }
 
-CandleBatcher.prototype.check = function() {
-  if(_.size(this.smallCandles) % this.candleSize !== 0)
-    return;
 
-  this.emitted++;
-  this.calculatedCandles.push(this.calculate());
-  this.smallCandles = [];
-}
+  write(candles) {
+    if (!_.isArray(candles)) {
+      throw new Error('candles is not an array');
+    }
 
-CandleBatcher.prototype.flush = function() {
-  _.each(
-    this.calculatedCandles,
-    candle => this.emit('candle', candle)
-  );
+    this.emitted = 0;
+    candles.forEach(candle => {
+      if (this.smallCandles) {
+        this.smallCandles.push(candle);
+      } else {
+        this.smallCandles = [candle];
+      }
+      this.check();
+    });
 
-  this.calculatedCandles = [];
-}
+    // _.each(candles, function (candle) {
 
-CandleBatcher.prototype.calculate = function() {
-  // remove the id property of the small candle
-  var { id, ...first } = this.smallCandles.shift();
+    //   this.check();
+    // }, this);
 
-  first.vwp = first.vwp * first.volume;
+    return this.emitted;
+  }
 
-  var candle = _.reduce(
-    this.smallCandles,
-    function(candle, m) {
-      candle.high = _.max([candle.high, m.high]);
-      candle.low = _.min([candle.low, m.low]);
-      candle.close = m.close;
-      candle.volume += m.volume;
-      candle.vwp += m.vwp * m.volume;
-      candle.trades += m.trades;
-      return candle;
-    },
-    first
-  );
 
-  if(candle.volume)
-    // we have added up all prices (relative to volume)
-    // now divide by volume to get the Volume Weighted Price
-    candle.vwp /= candle.volume;
-  else
-    // empty candle
-    candle.vwp = candle.open;
+  flush() {
+    _.each(
+      this.calculatedCandles,
+      candle => this.emit('candle', candle)
+    );
 
-  candle.start = first.start;
-  return candle;
+    this.calculatedCandles = [];
+  }
+
+  calculate() {
+    // remove the id property of the small candle
+    var { id, ...first } = this.smallCandles.shift();
+
+    first.vwp = first.vwp * first.volume;
+
+    var candle = _.reduce(
+      this.smallCandles,
+      function (candle, m) {
+        candle.high = _.max([candle.high, m.high]);
+        candle.low = _.min([candle.low, m.low]);
+        candle.close = m.close;
+        candle.volume += m.volume;
+        candle.vwp += m.vwp * m.volume;
+        candle.trades += m.trades;
+        return candle;
+      },
+      first
+    );
+
+    if (candle.volume)
+      // we have added up all prices (relative to volume)
+      // now divide by volume to get the Volume Weighted Price
+      candle.vwp /= candle.volume;
+    else
+      // empty candle
+      candle.vwp = candle.open;
+
+    candle.start = first.start;
+    return candle;
+  }
 }
 
 module.exports = CandleBatcher;
